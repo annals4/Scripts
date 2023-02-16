2c4e2825c1da9ca077f0a301c119174bb07762b1
annals4 <101326008+annals4@users.noreply.github.com>
2023-02-16 16:24:31 +0100

    Add files via upload

---
+++ ButtonsManager.cs
@@ -0,0 +1,277 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using static AB.AnyAllModel.FSM;
+using FSM = AB.AnyAllModel.FSM;
+using AB.FSMManager;
+using static AB.FSMManager.FSMManager;
+using System.Linq;
+using static System.Collections.Specialized.BitVector32;
+using System.Security.Cryptography;
+
+namespace AB.ButtonsManager
+{
+    public class ButtonsManager : MonoBehaviour
+    {
+        public static ButtonsManager Instance { get; private set; } = new ButtonsManager();
+
+        private bool andCondition = true;
+        private bool allCondition = true;
+
+        private string firedTransition = null;
+
+        public Dictionary<string, bool> buttons;
+        List<string> tags;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Initialize(FSM fsm, List<string> tagsList)
+        {
+            buttons = GetButtons(fsm.ListOfObjects);
+            tags = tagsList;
+        }
+
+        
+
+        public string ButtonTrigger(GameObject obj, FSMState currentState)
+        {
+            firedTransition = null;
+            foreach (var transition in currentState.ListOfTransitions)
+            {
+                //ConflictAnalyser(transition);
+                SettingType setting;
+                if (Enum.TryParse(transition.SettingType, out setting))
+                {
+                    ActionSetting(setting, transition, obj);
+
+                }
+
+            }
+            return firedTransition;
+        }
+
+        public void ActionSetting(SettingType setting, FSMTransition transition, GameObject obj)
+        {
+            string objId = obj.name;
+            switch (setting)
+            {
+                case SettingType.AND:///////////tutte le azioni di una transizione devono aver avuto luogo
+
+                    andCondition = true;
+                    foreach (var action in transition.ActionsOnTransition) //ciclo per settare se un azione è stata 'triggered'
+                    {
+                        var t = action.Target.Split(':');
+                        var tar = t[0];
+                        var excluded = t.Length > 1 ? t[1] : null;
+                        if (tar.Equals("ALL") | tar.Equals("ANY") | tags.Contains(tar))//se il Target è una parola speciale
+                        {
+                            TargetSetting(transition, action, obj, tar, excluded);
+                        }
+                        else //se il target è un oggetto qualsiasi
+                        {
+                            if (objId == action.Target) //entro nell'if solo se l'oggetto chiamante la funzione è il target dell'azione voluta
+                            {
+                                action.Triggered = true;
+                            }
+                        }
+                    }
+                    foreach (var action in transition.ActionsOnTransition) //ciclo per verificare che tutte le azioni siano state triggerate
+                    {
+                        if (!action.Triggered) //condizione per verificare che tutte le azioni della transizione siano state eseguite
+                        {
+                            andCondition = false;
+                            break;
+                        }
+                    }
+                    if (andCondition)
+                    {
+                        //this.fire
+                        firedTransition = transition.Name;
+                        UnTrigger(transition);
+                        ResetButtons(buttons);
+                        
+                    }
+
+                    break;
+                case SettingType.OR: ////////////solo un'azione qualsiasi della transizione deve essere stata fatta
+                    foreach (var action in transition.ActionsOnTransition)
+                    {
+                        var t = action.Target.Split(':');
+                        var tar = t[0];
+                        var excluded = t.Length > 1 ? t[1] : null;
+                        if (tar.Equals("ALL") | tar.Equals("ANY") | tags.Contains(tar))//se il Target è una parola speciale
+                        {
+                            TargetSetting(transition, action, obj, tar, excluded);
+                        }
+
+                        if (objId == action.Target)
+                        {
+                            action.Triggered = true;
+                            break;
+                        }
+                    }
+                    foreach (var action in transition.ActionsOnTransition)
+                    {
+                        if (action.Triggered)//me ne basta una
+                        {
+                            //this.machine.Fire(transition.Name);
+                            firedTransition = transition.Name;
+                            UnTrigger(transition);
+                            ResetButtons(buttons);  
+                            break;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+
+        
+
+        public void ResetButtons(Dictionary<string, bool> buttons)
+        {
+            foreach (var button in buttons.ToList()) //scorro tutti i bottoni
+            {
+                buttons[button.Key] = false;
+            }
+
+        }
+
+
+
+        public Dictionary<string, bool> GetButtons(List<FSMObj> objList) //restituisce la lista di tutti i bottoni in scena
+        {
+            Dictionary<string, bool> buttons = new Dictionary<string, bool>();
+            foreach (var obj in objList)
+            {
+                if (obj.Type.Equals("ButtonObject"))
+                {
+                    buttons.Add(obj.ObjectName, false);
+                }
+
+            }
+            return buttons;
+        }
+
+        ///////////////////////////////////////PARTE NUOVA******************************************
+
+        public void ButtonAction(FSMAction action, FSMTransition transition, GameObject obj)
+        {
+            //chiamo il metodo passandogli l'azione, la transizione, l'oggetto target
+            var t = action.Target.Split(':');
+            var tar = t[0];
+            var excluded = t.Length > 1 ? t[1] : null;
+
+            //PASSO 1: DISTINGUO IL CASO IN BASE AL TARGET
+            if (tar.Equals("ALL") | tar.Equals("ANY") | tags.Contains(tar))//se il Target è una parola speciale
+            {
+                TargetSetting(transition, action, obj, tar, excluded); //SETTA TRUE sull'azione triggerata
+            }
+            else //se il target è un oggetto qualsiasi
+            {
+                if (obj.name == action.Target) //entro nell'if solo se l'oggetto chiamante la funzione è il target dell'azione voluta
+                {
+                    action.Triggered = true;
+                }
+            }
+
+            //PASSO 2: HO ACTION TRIGGERED SETTATO. VEDO SE SONO TUTTE TRIGGERED SU ACTIONMANAGER
+        }
+
+        /// <summary>
+        /// Il target può essere:
+        /// - un oggetto qualsiasi
+        /// - le parole chiave ANY/ALL
+        /// - un gruppo di oggetti identificato con il tag
+        /// - ALL/tag meno un oggetto specifico o un tag specifico
+        /// </summary>
+        /// <param name="transition"></param>
+        /// <param name="action"></param>
+        /// <param name="objId"></param>
+        public void TargetSetting(FSMTransition transition, FSMAction action, GameObject obj, string target, string excluded)
+        {
+            string objId = obj.name;
+
+            buttons[objId] = true; //diventa vero quando pigio un bottone
+
+            switch (target)
+            {
+                case "ALL": //in questo caso quando clicco devo aver cliccato tutti i bottoni
+
+                    allCondition = true;
+
+                    if (excluded != null && !tags.Contains(excluded))
+                    {
+                        buttons[excluded] = true;
+                    }
+                    else if (tags.Contains(excluded))
+                    {
+                        foreach (var ex in GameObject.FindGameObjectsWithTag(excluded))
+                        {
+                            buttons[ex.name] = true;
+                        }
+                    }
+                    foreach (var button in buttons.ToList()) //scorro tutti i bottoni
+                    {
+                        if (!button.Value == true | !allCondition) //condizione per verificare se tutti i bottoni siano stati pigiati
+                        {
+                            allCondition = false;
+                            break; //appena allCondition diventa falsa non devo più andare avanti
+                        }
+
+                    }
+                    if (allCondition && !obj.tag.Equals(excluded))
+                    {
+                        action.Triggered = true;
+                    }
+                    break;
+                case "ANY": //qui quando clicco un bottone qualsiasi (ANY/But3), ANY/Modificabile
+
+                    allCondition = false;
+
+                    foreach (var button in buttons.ToList()) //scorro tutti i bottoni
+                    {
+                        if (button.Value == true && !objId.Equals(excluded) && !obj.tag.Equals(excluded)) //entro se pigio un bottone qualsiasi che però sia diverso da quello escluso
+                        {
+                            allCondition = true;
+                            break;
+                        }
+
+                    }
+                    if (allCondition)
+                    {
+                        action.Triggered = true;
+                    }
+                    break;
+                default: //NB: non ha senso escludere un tag da un altro tag (quindi tipo TAG1/TAG2) poiché i tag non si racchiudono
+                    if (obj.tag.Equals(target) && !objId.Equals(excluded)) //se il tag dell'oggetto cliccato è quello desiderato e l'oggetto non è escluso
+                    {
+                        action.Triggered = true;
+                    }
+                    break;
+            }
+
+
+        }
+
+        public void Reset()
+        {
+            ResetButtons(buttons);
+        }
+    }
+}
+
+
---
+++ Instatiator.cs
@@ -0,0 +1,122 @@
+using AB.AnyAllModel;
+using AB.Interactor;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static AB.FSMManager.FSMManager;
+
+namespace AB.Instatiator
+{
+    public class Instatiator : MonoBehaviour
+    {
+        
+        public GameObject RoundButtonPrefab;
+        public GameObject SquareButtonPrefab;
+        public GameObject CubePrefab;
+        public GameObject SpherePrefab;
+
+        private GameObject o;
+
+        public List<GameObject> ButtonObject  = new List<GameObject>();
+        public List<GameObject> ManipulableObject = new List<GameObject>();
+        public List<GameObject> SceneObject = new List<GameObject>();
+        public List<GameObject> TriggerObject = new List<GameObject>();
+        public static Instatiator Instance { get; private set; }
+
+        //TODO: capire se si possono aggiungere tag a runtime nel progetto
+
+        private void Awake()
+        {
+            FSM json = ParseJson("/Resources/Json/Manipulable.json");
+            Instance = this;
+
+            CheckMaterialCoherence(json);
+            foreach (var obj in json.ListOfObjects)
+            {
+                switch (obj.Tag) //da modificare questa parte
+                {
+                    case "SquareButton":
+                        o = Instantiate(SquareButtonPrefab, new Vector3(obj.Position.x, obj.Position.y, obj.Position.z), Quaternion.identity); //istanzia l'oggetto creando una copia di ButtonPrefab, alla posizione indicata, e con rotazione data
+                        break;
+                    case "RoundButton":
+                        o = Instantiate(RoundButtonPrefab, new Vector3(obj.Position.x, obj.Position.y, obj.Position.z), Quaternion.identity);
+                        break;
+                    case "CubeObject":
+                        o = Instantiate(CubePrefab, new Vector3(obj.Position.x, obj.Position.y, obj.Position.z), Quaternion.identity);
+                        break;
+                    case "SphereObject":
+                        o = Instantiate(SpherePrefab, new Vector3(obj.Position.x, obj.Position.y, obj.Position.z), Quaternion.identity);
+                        break;
+                    default:
+                        break;
+                }
+                o.name = obj.ObjectName;
+                o.transform.localScale = new Vector3(obj.Scale.x, obj.Scale.y, obj.Scale.z);
+                o.tag = obj.Tag;
+
+                switch (obj.Type)
+                {
+                    case "ManipulableObject":
+                        o.GetComponent<Renderer>().material = Resources.Load<Material>("Material/BaseMaterial/" + obj.Material);
+                        o.AddComponent<ObjectManipulator>();
+                        o.AddComponent<NearInteractionGrabbable>();
+                        o.GetComponent<Collider>().isTrigger = true;
+                        o.AddComponent<Rigidbody>();
+                        o.GetComponent<Rigidbody>().isKinematic = true;
+
+                        ManipulableObject.Add(o);
+                        break;
+                    case "TriggerObject":
+                        o.GetComponent<Renderer>().material = Resources.Load<Material>("Material/BaseMaterial/" + obj.Material);
+                        o.GetComponent<Collider>().isTrigger = true;
+                        o.AddComponent<Rigidbody>();
+                        o.GetComponent<Rigidbody>().isKinematic = true;
+                        TriggerObject.Add(o);
+                        break;
+                    case "ButtonObject":
+                        ButtonObject.Add(o);
+                        break;
+                    default:
+                        break;
+                }
+                SceneObject.Add(o); 
+                
+
+            }
+        }
+
+        public void CheckMaterialCoherence(FSM json) //verifica se i materiali degli oggetti esistono nella lista
+        {
+
+            foreach (var obj in json.ListOfObjects)
+            {
+                if (obj.Type == "ManipulableObject")
+                {
+                    if (!json.ListOfMaterials.Contains(obj.Material))
+                    {
+                        throw new InvalidOperationException("A ManipulableObject cannot have a Material that isn't on the ListOfMaterials ");
+                    }
+                    
+                }
+            }
+        }
+
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
